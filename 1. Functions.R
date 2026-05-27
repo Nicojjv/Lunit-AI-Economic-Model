a154a57b5a0510d05a761e8c559395d0c08f6faa
Nicojjv <156318027+Nicojjv@users.noreply.github.com>
2026-05-27 18:14:05 +0100

    1. Functions.R

--- 1. Functions.R
+++ 1. Functions.R
@@ -2,7 +2,7 @@
 ######           Functions         ######    
 #########################################
 
-dec_tree <- function(params, n_cohort, driver = "sensitivity") { 
+dec_tree <- function(params, n_cohort) { 
   with(
     as.list(params), 
     {
@@ -11,57 +11,24 @@ dec_tree <- function(params, n_cohort, driver = "sensitivity") {
       # ==========================================
       min_per_year <- 52 * 37.5 * 60  
       
+      p_disease <- prevalence 
+      p_healthy <- 1 - prevalence
+      
       # ==========================================
       #   1. ARM 1: USUAL CARE 
       # ==========================================
-      p_disease_a1 <- prevalence 
-      p_healthy_a1 <- 1 - p_disease_a1
-      
-      p_tp_a1   <- p_disease_a1 * arm1_sensitivity
-      p_fn_a1   <- p_disease_a1 * (1 - arm1_sensitivity) 
-      p_tn_a1   <- p_healthy_a1 * arm1_specificity
-      p_fp_a1   <- p_healthy_a1 * (1 - arm1_specificity)
+      p_tp_a1   <- p_disease * arm1_sensitivity
+      p_fn_a1   <- p_disease * (1 - arm1_sensitivity) 
+      p_tn_a1   <- p_healthy * arm1_specificity
+      p_fp_a1   <- p_healthy * (1 - arm1_specificity)
       
       # ==========================================
-      #   2. ARM 2: LUNIT AI (Dynamic Trade-off)
+      #   2. ARM 2: LUNIT AI 
       # ==========================================
-      
-      # A. Baseline Parameters 
-      base_sens <- 0.939 
-      base_spec <- 0.922
-      
-      # B. Diagnostic Odds Ratio (DOR) Calculation
-      dor_lunit <- (base_sens * base_spec) / ((1 - base_sens) * (1 - base_spec))
-      
-      # C. Dynamic Calculation based on Driver 
-
-      if (driver == "sensitivity") {
-        final_sens_a2 <- arm2_sensitivity
-        final_spec_a2 <- (dor_lunit * (1 - final_sens_a2)) / (final_sens_a2 + (dor_lunit * (1 - final_sens_a2)))
-        
-      } else if (driver == "specificity") {
-        final_spec_a2 <- arm2_specificity
-        final_sens_a2 <- (dor_lunit * (1 - final_spec_a2)) / (final_spec_a2 + (dor_lunit * (1 - final_spec_a2)))
-        
-      } else if (driver == "psa") {
-        final_sens_a2 <- arm2_sensitivity
-        final_spec_a2 <- arm2_specificity
-        
-      } else if(driver == "lunit cost") {
-        final_sens_a2 <- arm2_sensitivity
-        final_spec_a2 <- arm2_specificity
-        
-      } else {
-        stop("Error: argument 'driver' must be 'sensitivity', 'specificity', 'psa', or 'lunit cost'")
-      }
-      
-      p_disease_a2 <- prevalence 
-      p_healthy_a2 <- 1 - p_disease_a2
-      
-      p_tp_a2   <- p_disease_a2 * final_sens_a2
-      p_fn_a2   <- p_disease_a2 * (1 - final_sens_a2)
-      p_tn_a2   <- p_healthy_a2 * final_spec_a2
-      p_fp_a2   <- p_healthy_a2 * (1 - final_spec_a2)
+      p_tp_a2   <- p_disease * arm2_sensitivity
+      p_fn_a2   <- p_disease * (1 - arm2_sensitivity)
+      p_tn_a2   <- p_healthy * arm2_specificity
+      p_fp_a2   <- p_healthy * (1 - arm2_specificity)
       
       # ==========================================
       #   3. COST CALCULATIONS
@@ -80,7 +47,7 @@ dec_tree <- function(params, n_cohort, driver = "sensitivity") {
       
       # --- Costs Arm 2 ---
       cost_hr_a2  <- (1 * ((annual_salary_senior_radiologist/min_per_year) * reading_time_senior_radiologist)) 
-      cost_arb_a2 <- (arm1_p_arb * ((annual_salary_consultant_radiologist/min_per_year) * reading_time_consultant_radiologist))  
+      cost_arb_a2 <- (arm2_p_arb * ((annual_salary_consultant_radiologist/min_per_year) * reading_time_consultant_radiologist))  
       
       tc_tp_a2    <- cost_mammogram + cost_ultrasound + cost_hr_a2 + cost_arb_a2 + cost_lunit + cost_biopsy 
       tc_fn_a2    <- cost_mammogram + cost_ultrasound + cost_hr_a2 + cost_arb_a2 + cost_lunit  
@@ -104,10 +71,6 @@ dec_tree <- function(params, n_cohort, driver = "sensitivity") {
       df_results <- data.frame(
         Strategy             = c("Arm 1: Usual Care", "Arm 2: Lunit AI"),
         Cohort_Size          = c(n_cohort, n_cohort),
-           
-        Used_Sensitivity     = c(arm1_sensitivity, final_sens_a2),
-        Used_Specificity     = c(arm1_specificity, final_spec_a2),
-           
         TP                   = round(c(p_tp_a1 * n_cohort, p_tp_a2 * n_cohort)),
         FP                   = round(c(p_fp_a1 * n_cohort, p_fp_a2 * n_cohort)),
         FN                   = round(c(p_fn_a1 * n_cohort, p_fn_a2 * n_cohort)),
@@ -130,27 +93,20 @@ dec_tree <- function(params, n_cohort, driver = "sensitivity") {
   )
 }
 
-
-break_even_point <- function(target_value, params, n_cohort, param_name, driver = "sensitivity") {
-  
-  # 1. Dynamically update the specific parameter to evaluate
+break_even_point <- function(target_value, params, n_cohort, param_name) {
   params[[param_name]] <- target_value
-  
-  # 2. Run the tree passing the 'driver' argument
-  res <- dec_tree(params, n_cohort, driver = driver)
-  
-  # 3. Return the Incremental Cost (Cost AI - Cost Usual Care)
+  res <- dec_tree(params, n_cohort)
   diff <- res$Total_Cost[2] - res$Total_Cost[1]
-  
   return(diff)
 }
+
 
 # Standard error for probabilities => 95% = 0.01, remaining 0.1
 sim_p_psa <- function(mu, n_cohort = NULL, n_sims = 1000) {
   
   if (mu >= 1 | mu <= 0) return(rep(mu, n_sims))
   
-
+  
   if (!is.null(n_cohort)) {
     se <- sqrt((mu * (1 - mu)) / n_cohort)
   } else {
